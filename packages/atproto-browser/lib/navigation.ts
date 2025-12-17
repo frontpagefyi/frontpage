e44f83baf6fbe614389a9fdf9d8a09ea5911b7a7
Tom Sherman <tom@sherman.is>
2025-12-17 17:08:59 +0000

    Update atproto packages

--- packages/atproto-browser/lib/navigation.ts
+++ packages/atproto-browser/lib/navigation.ts
@@ -7,7 +7,7 @@ import { parse as parseHtml } from "node-html-parser";
 import { parse as parseLinkHeader } from "http-link-header";
 import { domainToASCII } from "url";
 import { isDid } from "@atproto/did";
-import { getRegistry as getLexiconRegistry } from "@lpm/core";
+import { getCatalog } from "@lpm/core";
 
 export async function navigateAtUri(input: string) {
   // Remove all zero-width characters and weird control codes from the input
@@ -179,8 +179,8 @@ async function getAtUriFromLexUri(
     };
   }
   const nsid = NSID.parse(uri.slice(4));
-  const registry = getLexiconRegistry();
-  const node = registry.get(nsid);
+  const catalog = getCatalog();
+  const node = catalog.get(nsid);
   const resolution = await node.resolve();
   if (!resolution.success) {
     return {
