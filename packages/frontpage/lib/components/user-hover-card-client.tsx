f281d6c9d9de335eb8c67f2116c92f232a2ece54
Tom Sherman <tom@sherman.is>
2025-12-29 22:26:46 +0000

    Improve composition of user hover card

--- packages/frontpage/lib/components/user-hover-card-client.tsx
+++ packages/frontpage/lib/components/user-hover-card-client.tsx
@@ -2,68 +2,35 @@
 
 import useSWR, { preload } from "swr";
 import { type DID } from "../data/atproto/did";
-import { HoverCardTrigger, HoverCardContent } from "./ui/hover-card";
-import { type ReactNode, Suspense } from "react";
-import { Skeleton } from "./ui/skeleton";
+import { HoverCardTrigger } from "./ui/hover-card";
+import { type ReactNode } from "react";
 import { ChatBubbleIcon, Link1Icon } from "@radix-ui/react-icons";
 import { type ApiRouteResponse } from "../api-route";
 import type { GET as GetHoverCardContent } from "@/app/api/hover-card-content/route";
 import Link from "next/link";
-import { ReportDialogIcon } from "@/app/(app)/_components/report-dialog";
-import { Separator } from "./ui/separator";
 
-type Props = {
-  did: DID;
-  children: ReactNode;
-  asChild?: boolean;
-  avatar: ReactNode;
-  handle: string | null;
-  reportAction: ((formData: FormData) => Promise<void>) | null;
-};
-
-export function UserHoverCardClient({
-  did,
+export function UserHoverCardTrigger({
   children,
   asChild,
-  avatar,
-  handle,
-  reportAction,
-}: Props) {
+  did,
+}: {
+  children: ReactNode;
+  asChild?: boolean;
+  did: DID;
+}) {
   return (
-    <>
-      <HoverCardTrigger
-        asChild={asChild}
-        onMouseEnter={() => {
-          void preload(did, getHoverCardData);
-        }}
-      >
-        {children}
-      </HoverCardTrigger>
-      <HoverCardContent className="w-80">
-        <div className="flex gap-4">
-          <Link href={`/profile/${handle ?? did}`} className="shrink-0">
-            {avatar}
-          </Link>
-          <div className="flex flex-col gap-1 basis-full">
-            <Suspense fallback={<Fallback handle={handle} did={did} />}>
-              <Content did={did} />
-            </Suspense>
-          </div>
-        </div>
-        {reportAction !== null ? (
-          <>
-            <Separator className="my-2" />
-            <div>
-              <ReportDialogIcon reportAction={reportAction} />
-            </div>
-          </>
-        ) : null}
-      </HoverCardContent>
-    </>
+    <HoverCardTrigger
+      asChild={asChild}
+      onMouseEnter={() => {
+        void preload(did, getHoverCardData);
+      }}
+    >
+      {children}
+    </HoverCardTrigger>
   );
 }
 
-function Content({ did }: { did: DID }) {
+export function UserHoverCardContent({ did }: { did: DID }) {
   const { data } = useSWR(did, getHoverCardData, {
     suspense: true,
     revalidateOnMount: false,
@@ -89,21 +56,6 @@ function Content({ did }: { did: DID }) {
       >
         <Link1Icon /> {data.postCount}
       </p>
-    </>
-  );
-}
-
-function Fallback({ handle, did }: { handle: string | null; did: DID }) {
-  return (
-    <>
-      <Link
-        href={`/profile/${handle ?? did}`}
-        className="text-sm font-semibold"
-      >
-        @{handle ?? "handle.invalid"}
-      </Link>
-      <Skeleton className="h-5 w-12" />
-      <Skeleton className="h-5 w-12" />
     </>
   );
 }
--- packages/frontpage/lib/components/user-hover-card.tsx
+++ packages/frontpage/lib/components/user-hover-card.tsx
@@ -1,11 +1,19 @@
 import { UserAvatar } from "./user-avatar";
-import { HoverCard } from "@/lib/components/ui/hover-card";
+import { HoverCard, HoverCardContent } from "@/lib/components/ui/hover-card";
 import { type DID } from "../data/atproto/did";
 import { getVerifiedHandle } from "../data/atproto/identity";
-import { UserHoverCardClient } from "./user-hover-card-client";
+import {
+  UserHoverCardContent,
+  UserHoverCardTrigger,
+} from "./user-hover-card-client";
 import { ensureUser, getUser } from "../data/user";
 import { parseReportForm } from "../data/db/report-shared";
 import { createReport } from "../data/db/report";
+import Link from "next/link";
+import { Skeleton } from "./ui/skeleton";
+import { Suspense } from "react";
+import { Separator } from "./ui/separator";
+import { ReportDialogIcon } from "@/app/(app)/_components/report-dialog";
 
 type Props = {
   did: DID;
@@ -20,21 +28,51 @@ export async function UserHoverCard({ did, children, asChild }: Props) {
 
   return (
     <HoverCard>
-      <UserHoverCardClient
-        avatar={<UserAvatar did={did} size="medium" />}
-        did={did}
-        asChild={asChild}
-        handle={handle}
-        reportAction={
-          user !== null ? reportUserAction.bind(null, { did }) : null
-        }
-      >
+      <UserHoverCardTrigger did={did} asChild={asChild}>
         {children}
-      </UserHoverCardClient>
+      </UserHoverCardTrigger>
+      <HoverCardContent className="w-80">
+        <div className="flex gap-4">
+          <Link href={`/profile/${handle ?? did}`} className="shrink-0">
+            <UserAvatar did={did} size="medium" />
+          </Link>
+          <div className="flex flex-col gap-1 basis-full">
+            <Suspense fallback={<ContentFallback handle={handle} did={did} />}>
+              <UserHoverCardContent did={did} />
+            </Suspense>
+          </div>
+        </div>
+        {user !== null ? (
+          <>
+            <Separator className="my-2" />
+            <div>
+              <ReportDialogIcon
+                reportAction={reportUserAction.bind(null, { did })}
+              />
+            </div>
+          </>
+        ) : null}
+      </HoverCardContent>
     </HoverCard>
   );
 }
 
+function ContentFallback({ handle, did }: { handle: string | null; did: DID }) {
+  return (
+    <>
+      <Link
+        href={`/profile/${handle ?? did}`}
+        className="text-sm font-semibold"
+      >
+        @{handle ?? "handle.invalid"}
+      </Link>
+      <Skeleton className="h-5 w-12" />
+      <Skeleton className="h-5 w-12" />
+    </>
+  );
+}
+
+// TODO: Find a better place for this action, it doesn't belong as part of the hover card component as it's used elsewhere. Or maybe just duplicate it?
 export async function reportUserAction(
   input: {
     did: DID;
