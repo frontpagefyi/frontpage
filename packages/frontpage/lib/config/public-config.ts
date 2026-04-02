939a07cbb0ca15924ad44866a1c7f1b767301361
Tom Sherman <tom@sherman.is>
2026-04-03 00:06:14 +0100

    Declare config

---
+++ packages/frontpage/lib/config/public-config.ts
@@ -0,0 +1,13 @@
+/* eslint-disable node/no-process-env */
+import z from "zod";
+
+const ServerEnv = z.object({
+  TURSO_CONNECTION_URL: z.string(),
+  NEXT_PUBLIC_DEFAULT_PDS_HOST: z.string().optional(),
+});
+
+export const publicConfig = ServerEnv.parse({
+  // We write these out manually so that turbo eslint can catch new env vars and ensure we add them to turbo.json
+  NEXT_PUBLIC_FEED_SERVICE_DID: process.env.NEXT_PUBLIC_FEED_SERVICE_DID,
+  NEXT_PUBLIC_DEFAULT_PDS_HOST: process.env.NEXT_PUBLIC_DEFAULT_PDS_HOST,
+});
