2346b8bb67df043d12bd430af6cf5db4d21c4633
Tom Sherman <tom@sherman.is>
2026-04-03 00:06:08 +0100

    Add eslint rule

--- packages/frontpage/eslint.config.mjs
+++ packages/frontpage/eslint.config.mjs
@@ -1,2 +1,19 @@
+import { defineConfig } from "eslint/config";
 import base from "@repo/eslint-config/next.js";
-export default base;
+import nodePlugin from "eslint-plugin-n";
+
+export default defineConfig(base, {
+  plugins: {
+    node: nodePlugin,
+  },
+  rules: {
+    // Ban process.env except process.env.NODE_ENV from all files
+    // We allow process.env.NODE_ENV to allow for bundle-time optimizations, putting indirection on it would prevent those optimizations from working
+    "node/no-process-env": [
+      "error",
+      {
+        allowedVariables: ["NODE_ENV"],
+      },
+    ],
+  },
+});
